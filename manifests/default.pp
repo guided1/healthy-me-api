0d8cb34073fd4e78df378805f11614b6b6ff217b
Gerard L. Petersen <gerard.petersen@gmail.com>
2015-06-11 15:35:22 +0200

    Added npm Distory kept up to date

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,29 +1,30 @@
 Exec {
-  path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
+  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
 
 class system-update {
   exec { 'apt-get update':
-    command => 'apt-get update',
+    command => 'apt-get update'
+  }
+
+  exec { 'apt-get upgrade':
+    command => 'apt-get upgrade -y',
+    require => Exec['apt-get update']
   }
 }
 
 class server {
-  $packages = ["nodejs", "mongodb"]
+  $packages = ['nodejs', 'mongodb', 'npm']
 
   package { $packages:
     ensure  => present,
-    require => Class["system-update"]
+    require => Class['system-update']
   }
 
   exec { 'update-alternatives node':
     command => 'update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10',
     require => Package[$packages]
   }
-}
-
-class node-packages {
-
 }
 
 require server
